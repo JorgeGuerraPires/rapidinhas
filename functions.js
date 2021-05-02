4cfe9c02215f97f5ba522e5b4bcca4ef07a3c31a
Jorge Guerra Pires <53444676+JorgeGuerraPires@users.noreply.github.com>
2021-05-02 16:56:15 -0300

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,11 @@
+
+
+const functions = {
+    add: (num1, num2) => num1 + num2 + 4,
+
+}
+
+//----------------------------
+//Necessário para exportar as funções
+module.exports = functions;
+//---------------------------
